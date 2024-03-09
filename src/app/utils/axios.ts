cb7547154078ccc5b68eb7e88c5bd93b9422a741
João Vitor Ianuci <joaoianuci@gmail.com>
2024-03-08 23:07:27 -0300

    feat: adding env file

--- src/app/utils/axios.ts
+++ src/app/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetcher = axios.create({
-  baseURL: "http://localhost:3333/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
